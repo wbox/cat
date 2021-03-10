108f9ec170e2e5ce49b190e6d114e5c4faf91687
Sam <sribas@gmail.com>
2021-03-10 11:59:40 +0000

    Week2/Day2 - Asynchronous Return Values

---
+++ asyncBreeds.js
@@ -0,0 +1,21 @@
+// asyncBreeds.js
+const fs = require('fs');
+
+const print = (data) => {
+  console.log("Retirn Value: ",data);
+};
+
+const breedDetailsFromFile = function(breed, callback) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log("In readFile's Callback: it has the data.");
+    // ISSUE: Returning from *inner* callback function, not breedDetailsFromFile.
+    if (!error) callback(data);
+  });
+  // ISSUE: Attempting to return data out here will also not work.
+  //        Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
+};
+
+// we try to get the return value
+const bombay = breedDetailsFromFile('Bombay',print);
+//console.log('Return Value: ', bombay); // => will NOT print out details, instead we will see undefined!
